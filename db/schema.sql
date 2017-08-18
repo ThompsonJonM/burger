fccc8d8dfa05c99acd0f07fcf654781468a2ce04
unknown <thompson.jonm@gmail.com>
2017-08-18 09:25:52 -0500

    db commit

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+create database burgers_db;
+
+use burgers_db;
+
+create table burgers (
+    id int auto_increment,
+    burger_name varchar(255) not null,
+    devoured boolean default false,
+    date timestamp,
+    primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+insert into burgers(burger_name) values ('Wahlburger');
+insert into burgers(burger_name) values ('Big Bopper');
+insert into burgers(burger_name) values ('Hollywood Burger');
+insert into burgers(burger_name) values ('Mushroom Swiss Burger');
+insert into burgers(burger_name) values ('Bleu Cheeseburger');
